786e34caa4c88f3bab112653dd1e2ef2e045ee76
Bernd Hahnebach <bernd@bimstatik.org>
2019-04-29 17:07:38 +0200

    updates in reihenhaus

--- scripts/reihenhaus.py
+++ scripts/reihenhaus.py
@@ -34,6 +34,17 @@ from FreeCAD import Vector as vec
 # tested on FreeCAD 0.19 dev (patched by Bernd)
 
 
+# TODO
+# error on first run
+# FreeCADGUi in if schleife
+# gebaeude und site erstellen
+# ifc types
+# properties
+# material
+# daemmung
+# wie kursaufbau ???
+# als FreeCAD macro abspeichern !!!
+
 # ***************************************************************************
 # how to run in a Python console in FreeCAD Gui or in FreeCADCmd
 
@@ -57,13 +68,13 @@ importlib.reload(reihenhaus)
 
 # ***************************************************************************
 # geometry input data, all units in mm
-eg_boden_fertig = 0.0
-eg_boden_roh = -200
-haus_t = 8000
-haus_b = 6000
-haus_h = 3000
+haus_t = 8000  # Aeusseres Rohbaumass
+haus_b = 6000  # Abstand zwischen Wandinnenseiten (nutzbare breite)
+haus_h = 3000  # OK roh BPL bis UK roh decke
 base_x = 0
 base_y = 0
+eg_boden_fertig = 0.0  # base_z
+eg_boden_roh = -200  # Bodenaufbau im EG
 trennwand_dicke = 300
 seitenwand_dicke = 175
 vorderwand_dicke = 125
@@ -85,6 +96,28 @@ doc_obj = FreeCAD.newDocument(doc_name)
 reihenhaus_laenge = haus_anzahl * (haus_b + trennwand_dicke) - trennwand_dicke + 2 * seitenwand_dicke
 hbase_x = base_x  # local x-base of every house, start with global x-base
 obj_ifc = []
+
+
+# *******************************************
+# bodenplatte
+bpl_place = FreeCAD.Placement(
+    vec(base_x-seitenwand_dicke, base_y, eg_boden_roh),
+    FreeCAD.Rotation(vec(0, 0, 1), 0)
+)
+bpl_base = Draft.makeRectangle(
+    length=reihenhaus_laenge,
+    height=haus_t,
+    placement=bpl_place
+)
+bpl_obj = Arch.makeStructure(
+    baseobj=bpl_base,
+    height=bpl_dicke
+)
+# structure will be extruded in positive z
+# thus set extrude Normale downwards
+bpl_obj.Normal = vec(0, 0, -1)
+doc_obj.recompute()
+obj_ifc.append(bpl_obj)
 
 
 for i, hs in enumerate(range(haus_anzahl)):
@@ -309,28 +342,6 @@ for i, hs in enumerate(range(haus_anzahl)):
     # *******************************************
     hbase_x += (haus_b + trennwand_dicke)
 doc_obj.recompute()
-
-
-# *******************************************
-# bodenplatte
-bpl_place = FreeCAD.Placement(
-    vec(base_x-seitenwand_dicke, base_y, eg_boden_roh),
-    FreeCAD.Rotation(vec(0, 0, 1), 0)
-)
-bpl_base = Draft.makeRectangle(
-    length=reihenhaus_laenge,
-    height=haus_t,
-    placement=bpl_place
-)
-bpl_obj = Arch.makeStructure(
-    baseobj=bpl_base,
-    height=bpl_dicke
-)
-# structure will be extruded in positive z
-# thus set extrude Normale downwards
-bpl_obj.Normal = vec(0, 0, -1)
-doc_obj.recompute()
-obj_ifc.append(bpl_obj)
 
 
 '''
